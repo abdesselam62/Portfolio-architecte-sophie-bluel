093aabbbe401c318f3d7545ff0ac763e81bed4a4
abdesselam62 <slamusdr@hotmail.com>
2023-08-04 21:20:45 +0200

    validé par W3C

--- FrontEnd/assets/script/modale.js
+++ FrontEnd/assets/script/modale.js
@@ -4,10 +4,10 @@ import { works, categories } from "./data.js";
 import { generateWorks } from "./gallery.js"
 
           
-    // Fonction qui gére l'affichage ou le masquage des fenêtres "modales" en fonction du "click"
+// Fonction qui gére l'affichage ou le masquage des fenêtres "modales" en fonction du "click"
 let modal = null;
-export function modale() {
 
+export function modale() {
     const openModalBtn = document.querySelectorAll(".open-modal-btn");
     const closeModalBtn = document.querySelectorAll(".close-modal-btn");
     const modal = document.querySelector(".modal");
@@ -25,15 +25,15 @@ export function modale() {
 
     // Bouton pour fermer la 1ére ou 2éme fenêtre modale
     if (modal === null) return;
-    for (let i = 0; i < closeModalBtn.length; i++) {
-        closeModalBtn[i].addEventListener("click", function (e) {
-            // Empêcher le comportement par défaut du clic
-            e.preventDefault();
-            modal.style.display = "none";
-            modal.setAttribute('aria-hidden', 'true')
-            modal.removeAttribute('aria-modal')
-        });
-    };
+        for (let i = 0; i < closeModalBtn.length; i++) {
+            closeModalBtn[i].addEventListener("click", function (e) {
+                // Empêcher le comportement par défaut du clic
+                e.preventDefault();
+                modal.style.display = "none";
+                modal.setAttribute('aria-hidden', 'true')
+                modal.removeAttribute('aria-modal')
+            });
+        };
 
    // Ferme la modale au click à l'extérieur de la 1ére ou 2éme fenêtre modale
     modal.addEventListener("click", function (e) {
@@ -47,35 +47,33 @@ export function modale() {
     });
 };
 
-    // Bouton pour ouvrir la 2éme fenêtre modale
 
+// Bouton pour ouvrir la 2éme fenêtre modale
     // Récupération de la class "addPhotoBtn" de "modalContent"
-    const addPhotoBtn = document.querySelector(".add-photo-btn");
+const addPhotoBtn = document.querySelector(".add-photo-btn");
     // Au click, masque la 1ére fenêtre modale et affiche la 2éme 
-    addPhotoBtn.addEventListener("click", function (e) {
-        // Empêcher le comportement par défaut du clic
-        e.preventDefault();
-        const modalContent = document.querySelector(".modal-content");
-        modalContent.style.display = "none";
-        const modalForm = document.querySelector(".modal-content-form");
-        modalForm.style.display = "flex";
-    });
+addPhotoBtn.addEventListener("click", function (e) {
+    // Empêcher le comportement par défaut du clic
+    e.preventDefault();
+    const modalContent = document.querySelector(".modal-content");
+    modalContent.style.display = "none";
+    const modalForm = document.querySelector(".modal-content-form");
+    modalForm.style.display = "flex";
+});
 
-    // Récupération de la class "returnModalBtn"
-    const returnModalBtn = document.querySelector(".return-modal-btn");
-    // Au click sur "returnModalBtn", retour sur "modalContent" (1ére fenêtre modale) 
-    returnModalBtn.addEventListener("click", function (e) {
-        // Empêcher le comportement par défaut du clic
-        e.preventDefault();
-        const modalContent = document.querySelector(".modal-content");
-        modalContent.style.display = "flex";
-        // et fermeture de la seconde fenêtre modale
-        const modalForm = document.querySelector(".modal-content-form");
-        modalForm.style.display = "none";
-    });
+// Récupération de la class "returnModalBtn"
+const returnModalBtn = document.querySelector(".return-modal-btn");
+// Au click sur "returnModalBtn", retour sur "modalContent" (1ére fenêtre modale) 
+returnModalBtn.addEventListener("click", function (e) {
+    // Empêcher le comportement par défaut du clic
+    e.preventDefault();
+    const modalContent = document.querySelector(".modal-content");
+    modalContent.style.display = "flex";
+    // et fermeture de la seconde fenêtre modale
+    const modalForm = document.querySelector(".modal-content-form");
+    modalForm.style.display = "none";
+});
 
-
- 
 // Fonction pour générer la "gallery" de la modale
 function generateWorksModale(works) {
     // Parcours des données "works" pour les ajouter au HTML de la Modale.
@@ -146,7 +144,7 @@ async function deleteWork(workId) {
         headers: {
         Authorization: "Bearer " + authToken,
         },
-      });
+    });
     
     // Si la réponse est "ok" alors le projet sera supprimé du DOM (Gallerie et modale)
     if (responseDelete.ok) {
@@ -164,7 +162,8 @@ async function deleteWork(workId) {
         // Supprime l'élément du tableau "works" à l'index "workIndexToRemove", "1" élément
         works.splice(workIndexToRemove, 1);
 
-    } else {
+    } else 
+    {
         return alert("Échec de la suppression du projet");
     };
 };
@@ -192,99 +191,104 @@ photoAddInput.addEventListener("change", function () {
         modalPreviewImg.addEventListener("click", function () {
             photoAddInput.click();
         });
-        } else {
+    } else 
+    {
         photoAddInput.value = "";
             return alert("La taille de l'image doit être inférieure à 4mo")
-        };
-        });
+    };
+});
 
-        // Copie du tableau de "categories" en enlevant l'index 0 (catégorie "Tous")
-        const categoriesModale = categories.slice(0);
+// Copie du tableau de "categories" en enlevant l'index 0 (catégorie "Tous")
+const categoriesModale = categories.slice(0);
 
-        // Parcours toutes les données de "categories"
-        for (let i = 0; i < categoriesModale.length; i++) {
-            const categoryModale = categoriesModale[i];
-            // Récupération de la class "work-category"
-            const workCategory = document.querySelector("#work-category");
-            // Création des balises "option"
-            const listScrollCategory = document.createElement("option");
-            // Récupération des "id"
-            listScrollCategory.value = categoryModale.id;
-            // Affichage de chaque nom de "categories"
-            listScrollCategory.innerText = categoryModale.name;
-            // "listScrollCategory" enfant de "work-category"
-            workCategory.appendChild(listScrollCategory);
-        };
-        // Récupération des deux "input" et du button "Valider"
-        const workTitle = document.querySelector("#work-title");
-        const workCategory = document.querySelector("#work-category");
-        const validForm = document.querySelector(".valid-form");   
-        
-        // Si les 3 inputs ont une valeur alors affiche le btn "validForm" en vert
-        function checkInputs() {
-            if (photoAddInput.value && workTitle.value && workCategory.value) {
-            validForm.classList.add("valid");
-            } else {
-            validForm.classList.remove("valid");
-            };
-        };
+// Parcours toutes les données de "categories"
+for (let i = 0; i < categoriesModale.length; i++) {
+    const categoryModale = categoriesModale[i];
+    // Récupération de la class "work-category"
+    const workCategory = document.querySelector("#work-category");
+    // Création des balises "option"
+    const listScrollCategory = document.createElement("option");
+    // Récupération des "id"
+    listScrollCategory.value = categoryModale.id;
+    // Affichage de chaque nom de "categories"
+    listScrollCategory.innerText = categoryModale.name;
+    // "listScrollCategory" enfant de "work-category"
+    workCategory.appendChild(listScrollCategory);
+};
 
-        photoAddInput.addEventListener("input", checkInputs);
-        workTitle.addEventListener("input", checkInputs);
-        workCategory.addEventListener("input", checkInputs);
+// Récupération des deux "input" et du button "Valider"
+const workTitle = document.querySelector("#work-title");
+const workCategory = document.querySelector("#work-category");
+const validForm = document.querySelector(".valid-form");   
 
-        // Au click sur le bouton "Valider" du "modalForm"
-        validForm.addEventListener("click", function (event) {
-        event.preventDefault();
-        // Si les 3 champs sont remplis
-        if (
-        photoAddInput.checkValidity() &&
-        workTitle.checkValidity() &&
-        workCategory.checkValidity() === true
-        ) {
-        // Appel de la fonction pour ajouter un projet
-        addWork();
-        } else {
-            return alert("Tous les champs sont requis");
-        };
+// Si les 3 inputs ont une valeur alors affiche le btn "validForm" en vert
+function checkInputs() {
+    if (photoAddInput.value && workTitle.value && workCategory.value) {
+    validForm.classList.add("valid");
+    } else {
+    validForm.classList.remove("valid");
+    };
+};
+
+photoAddInput.addEventListener("input", checkInputs);
+workTitle.addEventListener("input", checkInputs);
+workCategory.addEventListener("input", checkInputs);
+
+// Au click sur le bouton "Valider" du "modalForm"
+validForm.addEventListener("click", function (event) {
+    event.preventDefault();
+    // Si les 3 champs sont remplis
+    if (photoAddInput.checkValidity() && workTitle.checkValidity() && workCategory.checkValidity() === true) {
+    // Appel de la fonction pour ajouter un projet
+    addWork();
+    }     
+    else     
+    {
+        return alert("Tous les champs sont requis");
+    };
+});
+// Fonction ajouter un projet
+async function addWork() {
+    // Création d'un nouvel objet "FormData" pour stocker les données du formulaire
+    const formData = new FormData();
+
+    // Ajoute le fichier sélectionné "photoAddInput" au "formData"
+    formData.append("image", photoAddInput.files[0]);
+    // Ajoute la valeur saisie "workTitle" au "formData"
+    formData.append("title", workTitle.value);
+    // Ajoute la valeur "workCategory" choisie au "formData"
+    formData.append("category", workCategory.value);
+
+    const addResponse = await fetch("http://localhost:5678/api/works/", {
+    method: "POST",
+    headers: {
+        Authorization: "Bearer " + authToken,
+        accept: "application/json",
+    },
+    body: formData,
     });
-        // Fonction ajouter un projet
-        async function addWork() {
-        // Création d'un nouvel objet "FormData" pour stocker les données du formulaire
-        const formData = new FormData();
-    
-        // Ajoute le fichier sélectionné "photoAddInput" au "formData"
-        formData.append("image", photoAddInput.files[0]);
-        // Ajoute la valeur saisie "workTitle" au "formData"
-        formData.append("title", workTitle.value);
-        // Ajoute la valeur "workCategory" choisie au "formData"
-        formData.append("category", workCategory.value);
-    
-        const addResponse = await fetch("http://localhost:5678/api/works/", {
-        method: "POST",
-        headers: {
-            Authorization: "Bearer " + authToken,
-            accept: "application/json",
-        },
-        body: formData,
-        });
-    
-        // Si réponse "ok" alors on ajoute le projet au DOM
-        if (addResponse.ok) {
+
+    // Si réponse "ok" alors on ajoute le projet au DOM
+    if (addResponse.ok) {
         // Ajoute mon nouveau projet à la fin du tableau "works"
         works.push(await addResponse.json());
         returnModalBtn.click();
+
         // Réinitialisation du DOM
         const modalGallery = document.querySelector(".modal-gallery");
-        const sectionGallery = document.querySelector(".gallery");
+        const galleryWorks = document.querySelector(".gallery");
         modalGallery.innerHTML = "";
-        sectionGallery.innerHTML = "";
+        galleryWorks.innerHTML = "";
+        //stop du chargement de la page
+        window.stop(); 
         // Les galleries se mettent à jour avec les nouvelles valeurs
         generateWorksModale(works);
         generateWorks(works);
-            } else {
+    } 
+        else 
+    {
         return alert("Échec de l'ajout du projet");
-            };
-        };
-    
-    
+    };
+};
+
+
